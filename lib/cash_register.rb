42a4f04bcd172497ba648f2d49c5953b8969662a
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-08 01:00:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -49,7 +49,7 @@ class CashRegister
   # remove the last transaction from the total
   # return a total of 0 if all items are removed
   def void_last_transaction
-    binding.pry
+    # binding.pry
     if @items == []
       self.total == 0
     else self.total = @total - @transactions[-1]
