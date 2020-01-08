aeb3e6532eb26b61fa91fd8b0b18365e3f4944e2
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-08 00:49:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,7 +6,7 @@
 
 class CashRegister
 
-  attr_accessor :total, :discount, :last_transaction_item, :last_item
+  attr_accessor :total, :discount, :last_item
 
   def initialize(discount = 0)
     @total = 0
@@ -48,11 +48,11 @@ class CashRegister
   # remove the last transaction from the total
   # return a total of 0 if all items are removed
   def void_last_transaction
-    
+    self.total = @total - @items[-1]
   end
 
 
-# self.total = @total - @transactions.pop
+# 
 # @items = self.last_item
 
 end
